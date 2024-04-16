52333006bd95f83e637f63d81331b78613d5c60d
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-04-16 17:52:18 +0100

    add: mock data of weather

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,30 @@
+import weather from './weather.json'
 
 export default function Hero() {
+  console.log(JSON.stringify(weather, null, 4))
+  // include these
+      // { 
+      //   "id": 501,
+      //   "main": "Rain",
+      //   "description": "moderate rain",
+      //   "icon": "10d"
+      // }
+    // "main": {
+    //     "temp": 298.48,
+    //     "feels_like": 298.74,
+    //     "temp_min": 297.56,
+    //     "temp_max": 300.05,
+    //     "pressure": 1015,
+    //     "humidity": 64,
+    //     "sea_level": 1015,
+    //     "grnd_level": 933
+    // },
   return (
-    <div>
-      <h1>Hello World, Hello Weather App!</h1>
-    </div>  
+    <>
+      <pre>{JSON.stringify(weather, null, 4)}</pre>
+      <div>
+        Test
+      </div>
+    </>
   )
 }
