a289ef7f3849190d724d875b5a7b6e4da9c36757
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-04-29 19:18:18 +0100

    add: Roboto Mono font as a default font

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,5 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  fontFamily: 'Roboto Mono, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
 });
