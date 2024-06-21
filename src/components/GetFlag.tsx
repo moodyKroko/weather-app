618acb8d840bcf812f3a9257e5b67ce492ad9221
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-06-21 22:34:41 +0100

    add interface type as prop

--- src/components/GetFlag.tsx
+++ src/components/GetFlag.tsx
@@ -1,7 +1,11 @@
 import { Image } from '@mantine/core'
 
-export const GetFlag = (params: { countryName: string }) => {
-  const base_flag_url = `https://flagcdn.com/h24/${params.countryName.toLowerCase()}.png`
+interface GetFlagProps {
+  countryName: string
+}
+
+export const GetFlag = ({ countryName }: GetFlagProps) => {
+  const base_flag_url = `https://flagcdn.com/h24/${countryName.toLowerCase()}.png`
 
   return <Image h={24} w={34} radius="md" src={base_flag_url} />
 }
