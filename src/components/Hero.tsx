caa9cd097763fdb68ae3366acac6200f88280edf
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-06-28 17:21:01 +0100

    separated some logic to its own funnction and cleaned up code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,37 +1,91 @@
-import { useDebouncedState } from '@mantine/hooks'
+import { Container, Stack, Table } from '@mantine/core'
+import { useDebouncedCallback, useDebouncedState } from '@mantine/hooks'
+import { useState } from 'react'
+
+import { getSearchResults } from '../services/getServices'
+import { CountryCodes, SearchResults } from '../types'
 import Search from './Search'
-import weather from './weather.json'
 
-export default function Hero() {
-  console.log(JSON.stringify(weather, null, 4))
+import codes from './codes.json'
+import { GetFlag } from './GetFlag'
+
+export default function Hero(): JSX.Element {
   const [search, setSearch] = useDebouncedState('', 500)
+  const [loading, setLoading] = useState(false)
+  const [searchResults, setSearchResults] = useState<SearchResults[]>([])
+  const [weatherResult, setWeatherResult] = useState('')
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [countryNames] = useState<CountryCodes>(codes.codes)
+
+  const handleSearch = useDebouncedCallback(
+    async (query: string): Promise<void> => {
+      if (query === '' || query.trim().length <= 0) {
+        setSearchResults([])
+      }
+
+      if (query.length != 0) {
+        setLoading(true)
+        setSearchResults(await getSearchResults(query.trim()))
+        setLoading(false)
+      }
+    },
+    1000
+  )
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.currentTarget.value)
+    handleSearch(event.currentTarget.value)
   }
 
-  // include these
-  // {
-  //   "id": 501,
-  //   "main": "Rain",
-  //   "description": "moderate rain",
-  //   "icon": "10d"
-  // }
-  // "main": {
-  //     "temp": 298.48,
-  //     "feels_like": 298.74,
-  //     "temp_min": 297.56,
-  //     "temp_max": 300.05,
-  //     "pressure": 1015,
-  //     "humidity": 64,
-  //     "sea_level": 1015,
-  //     "grnd_level": 933
-  // },
+  // TODO: when clicked do a get request to get the weather data
+  // from given latitude and longitude
+  const handleTableRowClick = (result: SearchResults) => {
+    console.log('latitude:', result.lat, 'longitude:', result.lon)
+    console.log('Query result clicked:', result.name)
+    setWeatherResult(result.name)
+  }
+
+  const rows = searchResults.map((result, index) => {
+    const countryCode = result.country.toLowerCase()
+
+    return (
+      <Table.Tr key={index} onClick={() => handleTableRowClick(result)}>
+        <Table.Td>{result.name}</Table.Td>
+        <Table.Td>{countryNames[countryCode]}</Table.Td>
+        <Table.Td>{result.state}</Table.Td>
+        <Table.Td>
+          <GetFlag countryName={countryCode} />
+        </Table.Td>
+      </Table.Tr>
+    )
+  })
+
   return (
     <>
-      <Search query={search} onChange={handleChange} />
-      <pre>{JSON.stringify(weather, null, 4)}</pre>
-      <div>Test</div>
+      <Stack gap="lg">
+        <Search
+          query={search}
+          onQueryChange={handleQueryChange}
+          isLoading={loading}
+        />
+
+        {/* TODO: export to WeatherList component */}
+        <Table highlightOnHover highlightOnHoverColor='gray' horizontalSpacing="xl">
+          <Table.Thead>
+            <Table.Tr bg="orange">
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Country</Table.Th>
+              <Table.Th>State</Table.Th>
+              <Table.Th>Flag</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+
+        {/* TODO: This is a another component that shows the weather result */}
+        <p>{weatherResult}</p>
+        <br />
+      </Stack>
     </>
   )
 }
