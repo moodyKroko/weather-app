d557f28b33449a6c8940155372c6865c1b56c714
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-06-28 17:19:23 +0100

    add react query to test if needed

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import '@mantine/core/styles.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import { theme } from './theme'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-export default function App() {
+const queryClient = new QueryClient()
+
+export default function App(): JSX.Element {
   return (
     <MantineProvider theme={theme}>
-      <Navbar />
-      <main>
-        <Container pt={90} size="md" bg="cyan">
-          <Hero />
-        </Container>
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <main>
+          <Container pt={90} size="md" bg="cyan">
+            <Hero />
+          </Container>
+        </main>
+      </QueryClientProvider>
     </MantineProvider>
   )
 }
