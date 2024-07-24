27096597f57e305ec14cedf69d98abaf9ffc56cb
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-07-24 17:34:51 +0100

    add WeatherCardProps and display the weather

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,13 +1,29 @@
-import { Flex } from '@mantine/core'
+import { Flex, Text } from '@mantine/core'
+import { WeatherResult } from '../types'
 
-interface WeatherResultProp {
-  weatherResult: string
+interface WeatherCardProps {
+  weather: WeatherResult[]
 }
 
-export default function WeatherCard({ weatherResult }: WeatherResultProp) {
+export default function WeatherCard({ weather }: WeatherCardProps) {
+  const weatherRes = weather[0]
+
   return (
-    <Flex bg="teal">
-      <p>{weatherResult}</p>
+    <Flex
+      direction={{ base: 'column', sm: 'row' }}
+      gap="md"
+      bg={{ base: 'green', sm: 'yellow' }}
+    >
+      <Text>Longitude: {weatherRes.coord.lon}</Text>
+      <Text>Latitude: {weatherRes.coord.lat}</Text>
+      <Text>Weather ID: {weatherRes.weather[0].id}</Text>
+      <Text>Weather description: {weatherRes.weather[0].description}</Text>
+      <Text>Weather icon: {weatherRes.weather[0].icon}</Text>
+      <Text>Weather main: {weatherRes.main.temp}</Text>
+      <Text>Weather main: {weatherRes.main.feels_like}</Text>
+      <Text>Weather main: {weatherRes.main.temp_min}</Text>
+      <Text>Weather main: {weatherRes.main.temp_max}</Text>
+      <Text>Weather main: {weatherRes.main.humidity}</Text>
     </Flex>
   )
 }
