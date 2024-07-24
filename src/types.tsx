3ef9db83c11fa58a7ad876bd811cecf2567fcef8
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-07-24 17:35:40 +0100

    updates to global types

--- src/types.tsx
+++ src/types.tsx
@@ -16,53 +16,50 @@ export type Coordinates = {
   lat: number
 }
 
-// type WeatherData = {
-//   id: number
-//   main: string
-//   description: string
-//   icon: string
-// }
-//
-// type Main = {
-//   temp: number
-//   feels_like: number
-//   temp_min: number
-//   temp_max: number
-//   pressure: number
-//   humidity: number
-//   sea_level: number
-//   grnd_level: number
-// }
-//
-// type WindInfo = {
-//   speed: number
-//   deg: number
-//   gust: number
-// }
-//
-// export type WeatherResult = {
-//   coord: Coordinates
-//   weather: WeatherData[]
-//   base: string
-//   main: Main
-//   visibility: number
-//   wind: WindInfo
-//   rain: {
-//     string: number
-//   }
-//   clouds: {
-//     all: number
-//   }
-//   dt: number
-//   sys: {
-//     type: number
-//     id: number
-//     country: string
-//     sunrise: number
-//     sunset: number
-//   }
-//   timezone: number
-//   id: number
-//   name: string
-//   cod: number
-// }
+export type WeatherResult = {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: [
+    {
+      id: number
+      main: string
+      description: string
+      icon: string
+    },
+  ]
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+    sea_level: number
+    grnd_level: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+    gust: number
+  }
+  rain: Record<string, number>
+  clouds: {
+    all: number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
