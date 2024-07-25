5093d2d49311013b3bb86726f2eb2f01606e3791
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-07-25 20:39:10 +0100

    import newly created ToggleColorScheme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,5 @@
-import {
-  ActionIcon,
-  Group,
-  Title,
-  useComputedColorScheme,
-  useMantineColorScheme,
-} from '@mantine/core'
-import { IconMoon, IconSun } from '@tabler/icons-react'
-import clsx from 'clsx'
+import { Group, Title } from '@mantine/core'
+import ToggleColorScheme from './ToggleColorScheme'
 
 import classes from './navbar.module.css'
 
@@ -24,28 +17,5 @@ export default function Navbar() {
         <ToggleColorScheme />
       </Group>
     </header>
-  )
-}
-
-function ToggleColorScheme() {
-  const { setColorScheme } = useMantineColorScheme()
-  const computedColorScheme = useComputedColorScheme('light', {
-    getInitialValueInEffect: true,
-  })
-
-  return (
-    <ActionIcon
-      onClick={() =>
-        setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')
-      }
-      variant="gradient"
-      size={42}
-      radius="md"
-      gradient={{ from: 'blue', to: 'teal', deg: 90 }}
-      aria-label="Toggle color  scheme"
-    >
-      <IconSun className={clsx(classes.icon, classes.light)} stroke={1.5} />
-      <IconMoon className={clsx(classes.icon, classes.dark)} stroke={1.5} />
-    </ActionIcon>
   )
 }
