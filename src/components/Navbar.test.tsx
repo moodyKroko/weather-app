4ba3c9087192dabae5069011fd6b2385f2fcf2fa
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-07-25 20:06:57 +0100

    remove old test

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -1,12 +1,6 @@
 import { render, screen } from '@test-utils'
 import Navbar from './Navbar'
 
-test('renders content', () => {
-  render(<Navbar />)
-
-  const element = screen.getByText('Weather App')
-  expect(element).toBeDefined()
-})
 
 describe('Navbar component', () => {
   it('has a title', () => {
