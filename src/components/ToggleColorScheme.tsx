dce2fb15aa7f203ac476391261b5e52db1dfaf1e
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-08-02 16:18:04 +0100

    add animation when theme toggle button is clicked

--- src/components/ToggleColorScheme.tsx
+++ src/components/ToggleColorScheme.tsx
@@ -5,29 +5,45 @@ import {
 } from '@mantine/core'
 
 import { IconMoon, IconSun } from '@tabler/icons-react'
-
-import clsx from 'clsx'
-import classes from './navbar.module.css'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function ToggleColorScheme() {
-  const { setColorScheme } = useMantineColorScheme()
+  const { toggleColorScheme } = useMantineColorScheme()
   const computedColorScheme = useComputedColorScheme('light', {
     getInitialValueInEffect: true,
   })
 
+  const darkGradient = { from: 'indigo.5', to: 'violet.9', deg: 90 }
+  const lightGradient = { from: 'orange', to: 'pink', deg: 90 }
+
+  const currentGradient =
+    computedColorScheme === 'light' ? darkGradient : lightGradient
+
   return (
-    <ActionIcon
-      onClick={() =>
-        setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')
-      }
-      variant="gradient"
-      size={42}
-      radius="md"
-      gradient={{ from: 'blue', to: 'teal', deg: 90 }}
-      aria-label="Toggle color scheme"
-    >
-      <IconSun className={clsx(classes.icon, classes.light)} stroke={1.5} />
-      <IconMoon className={clsx(classes.icon, classes.dark)} stroke={1.5} />
-    </ActionIcon>
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={computedColorScheme}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.19 }}
+      >
+        <ActionIcon
+          onClick={toggleColorScheme}
+          variant="gradient"
+          size={42}
+          radius="md"
+          gradient={currentGradient}
+          aria-label="Toggle color scheme"
+        >
+          {computedColorScheme === 'light' ? (
+            <IconMoon size="1.5rem" stroke={1.8} />
+          ) : (
+            <IconSun size="1.7rem" stroke={1.8} />
+          )}
+        </ActionIcon>
+      </motion.div>
+    </AnimatePresence>
   )
 }
