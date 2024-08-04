60da0a0001e5dfe4a03d1effe0e4e03ce9d72803
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-08-04 23:48:03 +0100

    change white to off-white in theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,5 +2,6 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
-  fontFamily: 'Roboto Mono, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+  fontFamily: 'Roboto Mono, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+  white: 'var(--mantine-color-gray-2)'
 });
