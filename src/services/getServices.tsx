e891b0a143b8179869929da95a51eb28d1613e01
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-08-04 15:43:04 +0100

    remove testing function and add getWeather function

--- src/services/getServices.tsx
+++ src/services/getServices.tsx
@@ -1,106 +1,35 @@
-// import axios from 'axios'
-import { Coordinates, SearchResults } from '../types'
+import axios from 'axios'
+import { SearchResults, WeatherResult } from '../types'
 
-// const BASE_URL = 'https://api.openweathermap.org/'
+const BASE_URL = 'https://api.openweathermap.org/'
 
-// const API_KEY = 'd03603dd86831533506547968f9cdbeb' // process.env.REACT_APP_API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY // import.meta.env.VITE_API_KEY
 
+/** call openWeatherApi & geo search the query string for latitude and longitude
+* @param query - Input string to call geo location API
+* @returns An array of `SearchResults`
+*/
 const getSearchResults = async (query: string): Promise<SearchResults[]> => {
-  // const res = await axios.get(
-  //   `${BASE_URL}/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
-  // )
-  // {
-  //   "name": "Hounslow",
-  //   "lat": 17.9354996,
-  //   "lon": -77.7331531,
-  //   "country": "JM"
-  // }
-  // this is for testing purpose
-  console.log("Query sent to external API:", query)
-  return new Promise(resolve =>
-    resolve([
-      {
-        name: 'Hounslow',
-        local_names: {
-          ur: 'ہنزلو',
-          en: 'Hounslow',
-        },
-        lat: 51.4686132,
-        lon: -0.3613471,
-        country: 'GB',
-        state: 'England',
-      },
-      {
-        name: 'Hou Hounslow 2 Test',
-        local_names: {
-          ur: 'ہنزلو',
-          en: 'Hounslow',
-        },
-        lat: 52.4686132,
-        lon: -1.3613471,
-        country: 'GB',
-        state: 'England',
-      },
-    ])
+  const res = await axios.get(
+    `${BASE_URL}/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
   )
+
+  console.log('Query sent to external API:', query)
+  return res.data
 }
 
-const getWeather = async ({lat, lon}: Coordinates) => {
-  console.log("Coordinates from getWeather Request:", lat, lon)
-
-  return new Promise(resolve =>
-    resolve([
-      {
-        coord: {
-          lon: 10.99,
-          lat: 44.34,
-        },
-        weather: [
-          {
-            id: 501,
-            main: 'Rain',
-            description: 'moderate rain',
-            icon: '10d',
-          },
-        ],
-        base: 'stations',
-        main: {
-          temp: 298.48,
-          feels_like: 298.74,
-          temp_min: 297.56,
-          temp_max: 300.05,
-          pressure: 1015,
-          humidity: 64,
-          sea_level: 1015,
-          grnd_level: 933,
-        },
-        visibility: 10000,
-        wind: {
-          speed: 0.62,
-          deg: 349,
-          gust: 1.18,
-        },
-        rain: {
-          '1h': 3.16,
-        },
-        clouds: {
-          all: 100,
-        },
-        dt: 1661870592,
-        sys: {
-          type: 2,
-          id: 2075663,
-          country: 'IT',
-          sunrise: 1661834187,
-          sunset: 1661882248,
-        },
-        timezone: 7200,
-        id: 3163858,
-        name: 'Zocca',
-        cod: 200,
-      },
-    ])
+/** call openWeatherApi and input lat and lon to get weather result
+* @param lat - latitude of the location
+* @param lon - longitude of the location
+* @returns `WeatherResult` object
+*/
+const getWeather = async (lat: number, lon: number): Promise<WeatherResult> => {
+  const res = await axios.get(
+    `${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
   )
+
+  console.log('Coordinates sent....Requesting weather data.', lat, lon)
+  return res.data
 }
 
 export { getSearchResults, getWeather }
