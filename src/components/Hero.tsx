9a813d0dbecc455a4ebb1656a01864f9eb377923
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-09-01 21:56:45 +0100

    update: use tanstack query to fetch weather data and cleanups

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,41 +1,25 @@
 import { Stack } from '@mantine/core'
-import { useDebouncedCallback, useDebouncedState } from '@mantine/hooks'
+import { useDebouncedState } from '@mantine/hooks'
 import { useState } from 'react'
 
-import { getSearchResults, getWeather } from '../services/getServices'
+import { getWeather } from '../services/getServices'
 import { SearchResults, WeatherResult } from '../types'
 import Search from './Search'
 
 import WeatherCard from './WeatherCard'
 import WeatherList from './WeatherList'
 
+
 export default function Hero(): JSX.Element {
-  const [search, setSearch] = useDebouncedState('', 500)
-  const [loading, setLoading] = useState(false)
-  const [searchResults, setSearchResults] = useState<SearchResults[]>([])
+  const [debouncedSearch, setDebouncedSearch] = useDebouncedState('', 500)
 
   const [isQueryClicked, setIsQueryClicked] = useState(false)
   const [clickedQuery, setClickedQuery] = useState<WeatherResult>()
 
-  const handleSearchInput = useDebouncedCallback(
-    async (query: string): Promise<void> => {
-      if (query === '' || query.trim().length <= 0) {
-        setSearchResults([])
-        setIsQueryClicked(false)
-      }
-
-      if (query.length != 0) {
-        setLoading(true)
-        setSearchResults(await getSearchResults(query.trim()))
-        setLoading(false)
-      }
-    },
-    1000
-  )
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.currentTarget.value)
-    handleSearchInput(event.currentTarget.value)
+    setDebouncedSearch(event.currentTarget.value)
   }
 
   const handleTableRowClick = (result: SearchResults) => {
@@ -47,17 +31,19 @@ export default function Hero(): JSX.Element {
     <>
       <Stack gap="lg">
         <Search
-          query={search}
+          query={debouncedSearch}
           onQueryChange={handleQueryChange}
-          isLoading={loading}
+          isLoading={isLoading}
         />
 
-        <WeatherList
-          querySearchResult={searchResults}
-          onTableRowClick={handleTableRowClick}
-        />
+          <WeatherList
+            searchQuery={debouncedSearch}
+            onTableRowClick={handleTableRowClick}
+          />
 
-        {isQueryClicked && <WeatherCard weather={clickedQuery} />}
+        {debouncedSearch && isQueryClicked && (
+          <WeatherCard weather={clickedQuery} />
+        )}
         <br />
       </Stack>
     </>
--- src/components/WeatherList.tsx
+++ src/components/WeatherList.tsx
@@ -1,10 +1,12 @@
-import { Skeleton, Table, Text } from '@mantine/core'
+import { Flex, Skeleton, Text } from '@mantine/core'
+import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
+import { getSearchResults } from '../services/getServices'
 import { CountryCodes, SearchResults } from '../types'
 import { GetFlag } from './GetFlag'
 import codes from './codes.json'
-import { getSearchResults } from '../services/getServices'
-import { useQuery } from '@tanstack/react-query'
+
+import styles from './Weatherlist.module.css'
 
 interface WeatherListProp {
   searchQuery: string
@@ -17,22 +19,50 @@ export default function WeatherList({
 }: WeatherListProp) {
   const [countryNames] = useState<CountryCodes>(codes.codes)
 
-  const { isPending, isLoading, data } = useQuery<SearchResults[]>({
+  const { isLoading, data } = useQuery<SearchResults[]>({
     queryKey: ['searchResults', searchQuery],
     queryFn: () => getSearchResults(searchQuery),
     enabled: Boolean(searchQuery),
   })
 
-  console.log('Query loading before undefined check:', isLoading)
-  console.log('Query loading before undefined check:', isPending)
+  const renderSkeletons = () => {
+    return Array.from({ length: 5 }).map((_, index) => (
+      <Skeleton key={index} height={50} />
+    ))
+  }
 
   console.log(data)
 
   return (
     <>
-      {/* const countryCode = result.country.toLowerCase() */}
-      {/* const { name, state } = result */}
-      <Text>fasdfs</Text>
+      {isLoading ? (
+        <>{renderSkeletons()}</>
+      ) : (
+        <div>
+          {data?.map((place, index) => {
+            const isState = place.state === undefined
+            const country = countryNames[place.country.toLowerCase()]
+
+            return (
+              <div key={index} className={styles.roundedEdges}>
+                <Flex
+                  direction={{ base: 'column', sm: 'row' }}
+                  justify={{ sm: 'flex-start' }}
+                  align={{ base: 'flex-start', sm: 'center' }}
+                  p="md"
+                  gap="md"
+                  wrap="wrap"
+                >
+                  <Text>{place.name},</Text>
+                  {isState ? '' : <Text>{place.state},</Text>}
+                  <Text>{country}</Text>
+                  <GetFlag countryName={place.country} />
+                </Flex>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </>
   )
 }
