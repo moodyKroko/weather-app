361d64d9d0a4185725a5a68cd19410f5d0eb3c4e
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-09-01 21:53:24 +0100

    update: remove avatar and instead use image

--- src/components/GetFlag.tsx
+++ src/components/GetFlag.tsx
@@ -1,11 +1,11 @@
-import { Avatar } from '@mantine/core'
+import { Image } from '@mantine/core'
 
 interface GetFlagProps {
   countryName: string
 }
 
 export const GetFlag = ({ countryName }: GetFlagProps) => {
-  const base_flag_url = `https://flagcdn.com/h24/${countryName.toLowerCase()}.png`
+  const base_flag_url = `https://flagcdn.com/40x30/${countryName.toLowerCase()}.png`
 
-  return <Avatar radius="sm" src={base_flag_url} />
+  return <Image h={30} w={40} src={base_flag_url} />
 }
