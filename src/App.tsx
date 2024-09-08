37033a1c2c798b2277ab8bcb890852b30eedb2bb
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-09-08 16:45:21 +0100

    cleanup imports and remove unnecessary containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { Container, MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import { theme } from './theme'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
 export default function App(): JSX.Element {
+
   return (
     <MantineProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Navbar />
         <main>
-          <Container pt={46} size="md">
-            <Hero />
-          </Container>
+          <Hero />
         </main>
       </QueryClientProvider>
     </MantineProvider>
