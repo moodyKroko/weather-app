2ba1dc75495d22782f3e025fc0dacf2da3fc18f9
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-09-09 12:00:18 +0100

    remove redundant code

--- src/theme.ts
+++ src/theme.ts
@@ -4,5 +4,4 @@ export const theme = createTheme({
   /* Put your mantine theme override here */
   fontFamily:
     'Roboto Mono, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
-  // white: 'var(--mantine-color-gray-3)',
 })
